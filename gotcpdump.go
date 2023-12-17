a5200951dc388190742fff46fd1cc3322519ea6a
Blade <mjc@gabia.com>
2023-12-18 08:53:13 +0900

    test

--- gotcpdump.go
+++ gotcpdump.go
@@ -191,6 +191,8 @@ func startCapturePacket(c *cli.Context) bool {
 					common.PrintFlow(srcIP, srcPort, dstIP, dstPort)
 					common.PrintEtherLayer(nic, srcMac, dstMac)
 					common.PrintTcpLayer(tcp.Seq)
+
+					fmt.Println(string(tcp.Payload()))
 				case layers.LayerTypeUDP:
 					srcPort = udp.SrcPort.String()
 					dstPort = udp.DstPort.String()
